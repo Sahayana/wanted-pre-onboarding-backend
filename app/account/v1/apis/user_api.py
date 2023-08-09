fb7c1de5132795fb9eebfdecb2fb4c549453f27c
sahayana <saokhueyang@gmail.com>
2023-08-09 21:30:58 +0900

    추가: Login api 유효성 검사 추가

--- app/account/v1/apis/user_api.py
+++ app/account/v1/apis/user_api.py
@@ -4,6 +4,7 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+from app.account.helpers import email_validater, password_validater
 from app.account.models import User
 from app.account.v1.serializers.user_serializer import UserCreateSerializer
 
@@ -24,7 +25,8 @@ class UserCreateApiView(CreateAPIView):
 
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
+            user = serializer.save()
+            request.user = user
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(
             data=serializer.error_messages, status=status.HTTP_400_BAD_REQUEST
@@ -48,14 +50,23 @@ class LoginView(views.APIView):
 
         user = authenticate(email=email, password=password)
 
+        if not email_validater(email=email):
+            return Response(
+                data={"error": "잘못된 이메일 양식입니다."}, status=status.HTTP_400_BAD_REQUEST
+            )
+        if not password_validater(password=password):
+            return Response(
+                data={"error": "비밀번호는 8자 이상입니다."}, status=status.HTTP_400_BAD_REQUEST
+            )
+
         if not user:
             return Response(
-                data={"error": "Login failed"}, status=status.HTTP_400_BAD_REQUEST
+                data={"error": "존재하지 않는 유저입니다."}, status=status.HTTP_400_BAD_REQUEST
             )
 
         access_token, refresh_token = self.obtain_token(user=user)
 
         return Response(
-            data={"access token": access_token, "refresh token": refresh_token},
+            data={"access_token": access_token, "refresh_token": refresh_token},
             status=status.HTTP_200_OK,
         )
